f4495b3d7ac301a2d030b1e0c1027f1f3139921f
RamSailopal <thelinuxguys@yahoo.com>
2022-01-17 16:05:20 +0000

    Had to build Adafruit_Python_DHT from repo source to allow the force on pi architecture

--- sensor-entrypoint/entrypoint.sh
+++ sensor-entrypoint/entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+yum update -y
+yum install -y git gcc
+cd /usr/local
+git clone https://github.com/adafruit/Adafruit_Python_DHT.git
+cd /usr/local/Adafruit_Python_DHT
+python3 setup.py install --force-pi
 python3 -m pip install paho-mqtt
-python3 -m pip install Adafruit_DHT
 cd /usr/local/YottaDB-IoT
 ./mqttsend.py &
 sleep 1
 ./mqttrec.py &
-./mqttrec1.py &
+./mqttrec1.py & 
 tail -f /dev/null
