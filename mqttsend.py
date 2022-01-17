95e8e078d52027e041ff2a6cf5a3d39eb8792020
RamSailopal <thelinuxguys@yahoo.com>
2022-01-17 16:03:50 +0000

    Added logic for running with dummy sensor data if not PI available

--- mqttsend.py
+++ mqttsend.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import paho.mqtt.client as mqtt 
 import time
+import os
 from datetime import timezone
 from datetime import date
 from datetime import datetime
 
 mqttBroker ="mqtt" 
 
-client = mqtt.Client("Temperature_Inside")
+client = mqtt.Client("Sensor")
 client.connect(mqttBroker) 
 
 while True:
@@ -18,13 +19,21 @@ while True:
     hr = today.strftime("%H")
     min = today.strftime("%M")
     sec = today.strftime("%S")
-    #timestamp=yr + "-" + mn + "-" + dy + " " + hr + ":" + min + ":" + sec
     timestamp=today.strftime("%Y-%m-%dT%H:%M:%S.000Z")
-    #dt = datetime(int(yr), int(mn), int(dy), int(hr), int(min), int(sec))
-    #timestamp = dt.replace(tzinfo=timezone.utc).timestamp()
-    #timestamp=int(timestamp)
-    client.publish("yotta/db/temp", str(timestamp) + "#7")
-    print("Just published " + str(7) + " to topic yotta/db/temp")
-    client.publish("yotta/db/humid", str(timestamp) + "#23")
-    print("Just published " + str(23) + " to topic yotta/db/humid")
-    time.sleep(1)
+    if (os.getenv('NOPI')=="1"):
+       client.publish("yotta/db/temp", str(timestamp) + "#7")
+       print("Just published " + str(7) + " to topic yotta/db/temp")
+       client.publish("yotta/db/humid", str(timestamp) + "#23")
+       print("Just published " + str(23) + " to topic yotta/db/humid")
+    else:
+       sensor=Adafruit_DHT.DHT11
+       gpio=17
+       humidity, temperature = Adafruit_DHT.read_retry(sensor, gpio)
+       if humidity is not None and temperature is not None:
+          print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+          client.publish("yotta/db/temp", str(timestamp) + "{0:0.1f}".format(temperature))
+          client.publish("yotta/db/temp", str(timestamp) + "{0:0.1f}".format(humidity))
+       else:
+           print('Failed to get reading. Try again!')
+ 
+    time.sleep(60)
