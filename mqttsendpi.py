be0e096d74e1d38abf3cbf6022ff57a2c7286adb
RamSailopal <thelinuxguys@yahoo.com>
2022-01-18 12:22:22 +0000

    Split message send logic into logic for Pi and logic for dummy no pi

--- mqttsendpi.py
+++ mqttsendpi.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import paho.mqtt.client as mqtt 
+import Adafruit_DHT
 import time
 import os
 import sys
@@ -15,11 +16,13 @@ if (sys.argv[1]==""):
    print("You must pass the address of the mqtt server")
    sys.exit(1)
 
+if (len(sys.argv)<3):
+   port=1883
 
 mqttBroker = sys.argv[1] 
 
 client = mqtt.Client("Sensor")
-client.connect(mqttBroker) 
+client.connect(mqttBroker,port) 
 
 while True:
     today = datetime.now()
@@ -30,9 +33,14 @@ while True:
     min = today.strftime("%M")
     sec = today.strftime("%S")
     timestamp=today.strftime("%Y-%m-%dT%H:%M:%S.000Z")
-    client.publish("yotta/db/temp", str(timestamp) + "#7")
-    print("Just published " + str(7) + " to topic yotta/db/temp")
-    client.publish("yotta/db/humid", str(timestamp) + "#23")
-    print("Just published " + str(23) + " to topic yotta/db/humid")
+    sensor=Adafruit_DHT.DHT11
+    gpio=17
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, gpio)
+    if humidity is not None and temperature is not None:
+       print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+       client.publish("yotta/db/temp", str(timestamp) + "{0:0.1f}".format(temperature))
+       client.publish("yotta/db/temp", str(timestamp) + "{0:0.1f}".format(humidity))
+    else:
+       print('Failed to get reading. Try again!')
  
     time.sleep(60)
