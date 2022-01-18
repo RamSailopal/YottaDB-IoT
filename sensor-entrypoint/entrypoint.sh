ef4f0a2e24da2f030d5a976be24494446e22bbbc
RamSailopal <thelinuxguys@yahoo.com>
2022-01-18 11:53:48 +0000

    Now passing variable for mqtt server as the send process is now going to be run outside of compose

--- sensor-entrypoint/entrypoint.sh
+++ sensor-entrypoint/entrypoint.sh
@@ -7,8 +7,6 @@ cd /usr/local/Adafruit_Python_DHT
 python3 setup.py install --force-pi
 python3 -m pip install paho-mqtt
 cd /usr/local/YottaDB-IoT
-./mqttsend.py &
-sleep 1
 ./mqttrec.py &
 ./mqttrec1.py & 
 tail -f /dev/null
