ecf85db966dd0373c9e0fa4172ccf0d27c69245f
RamSailopal <thelinuxguys@yahoo.com>
2022-01-18 15:08:26 +0000

    no delimiter between timestamp and value

--- mqttsendpi.py
+++ mqttsendpi.py
@@ -27,8 +27,8 @@ while True:
     humidity, temperature = Adafruit_DHT.read_retry(sensor, gpio)
     if humidity is not None and temperature is not None:
        print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
-       client.publish("yotta/db/temp", str(timestamp) + "{0:0.1f}".format(temperature))
-       client.publish("yotta/db/temp", str(timestamp) + "{0:0.1f}".format(humidity))
+       client.publish("yotta/db/temp", str(timestamp) + "#" + "{0:0.1f}".format(temperature))
+       client.publish("yotta/db/humid", str(timestamp) + "#" + "{0:0.1f}".format(humidity))
     else:
        print('Failed to get reading. Try again!')
  
